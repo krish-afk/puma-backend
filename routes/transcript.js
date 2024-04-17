ef9d5ee292a7963261d3af0cec4865c34648f66d
fgormley1 <fgormley2@gmail.com>
2024-04-16 21:11:28 -0400

    did a thing

--- routes/transcript.js
+++ routes/transcript.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const { uploadTranscript } = require('../controllers/uploadTranscript.js');
+const multer = require('multer');
 
+const storage = multer.memoryStorage(); // or you can use diskStorage for saving files to disk
+const upload = multer({ storage: storage });
 
-router.post('/upload', uploadTranscript);
+router.post('/upload', (req, res, next) => {
+    console.log("Received upload request");
+    next();
+}, upload.single('file'), (req, res, next) => {
+    console.log("File passed through multer");
+    next();
+}, uploadTranscript);
 
 module.exports = router;
 
