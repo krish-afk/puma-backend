ac18533259acbd18fa73d2cab3f3653c04b08997
krish-afk <84220215+krish-afk@users.noreply.github.com>
2024-04-18 01:04:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ app.use((req, res, next) => {
 
 app.use(cors(corsOptions));
 
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','*')
-    res.setHeader('Access-Control-Allow-Methods','GET, POST , PUT,DELETE')
-    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization')
-    console.log("CORS headers set")
-    next();
-});
+// app.use((req,res,next)=>{
+//     res.setHeader('Access-Control-Allow-Origin','*')
+//     res.setHeader('Access-Control-Allow-Methods','GET, POST , PUT,DELETE')
+//     res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization')
+//     console.log("CORS headers set")
+//     next();
+// });
 
 
 
