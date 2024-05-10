ca9e29dca0850794cd48fc8e06f63ac954a6f364
Vani Gupta <59418361+TheTrailblazer09@users.noreply.github.com>
2024-05-10 02:40:32 -0400

    added route to get user

--- routes/students.js
+++ routes/students.js
@@ -3,6 +3,7 @@ const router= express.Router();
 const controller= require('../controllers/students')
 
 router.post('/createUser',controller.createUser)
+router.get('/getUser',controller.getUser)
 router.post('/authenticateUser',controller.authenticateUser)
 
 module.exports= router;
